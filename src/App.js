e46e9aed6e558099376e3ded4b3671c0306f86ee
Jared Siirila <siirila@gmail.com>
2018-07-26 18:30:48 -0400

    Break time up into hours, minutes, seconds

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import Clock from './Clock.js';
 import Stopwatch from './Stopwatch.js';
 
 
-let removeClock = false;
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {!removeClock && <Clock />}
-
-        { !removeClock &&
-          <button type="button" onClick={() => {removeClock = true; this.forceUpdate()}}>Remove clock</button>
-        }
-        { removeClock &&
-          <button type="button" onClick={() => {removeClock = false; this.forceUpdate()}}>Add clock</button>
-        }
+        <h2>Stopwatch</h2>
         <br />
         <Stopwatch />
       </div>
--- src/TimeDisplay.js
+++ src/TimeDisplay.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+const hour = 1000 * 60 * 60;
+const minute = 1000 * 60;
+
 export default function TimeDisplay(props) {
-    return <div>{props.time} milliseconds</div>;
+    let time = props.time;
+    let hours = Math.floor(time / hour);
+    time = time % hour;
+    let minutes = Math.floor(time / minute);
+    time = time % minute;
+    let seconds = Math.floor(time / 1000);
+    let tenthsOfASecond = Math.floor((time % 1000)/100);
+
+    return <div>{`${hours}:${minutes}:${seconds}::${tenthsOfASecond}`}</div>;
 }
